579afb23500c2bbbf34e700d8457bc0edc65fabf
weiyi <weiyiwang@togocareer.com>
2024-02-20 18:06:39 +0800

    test

---
+++ copier/README.md
@@ -0,0 +1,13 @@
+# common
+
+## Features
+
+* Copy from field to field with same name
+* Copy from method to field with same name
+* Copy from field to method with same name
+* Copy from slice to slice
+* Copy from struct to slice
+* Copy from map to map
+* Enforce copying a field with a tag
+* Ignore a field with a tag
+* Deep Copy
---
+++ copier/copierx.go
@@ -0,0 +1,63 @@
+package copier
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+func Struct2Json(from interface{}) (err error) {
+	str, err := json.Marshal(from)
+	if err != nil {
+		return
+		//log.Warn("Struct2Json, can not convert: %v", err)
+	}
+	from = string(str)
+	// return string(str), err
+	return
+}
+
+func Json2Struct(to interface{}, from string) (err error) {
+	err = json.Unmarshal([]byte(from), to)
+	if err != nil {
+		return
+		//log.Warn("Json2Struct, can not convert: %v", err)
+	}
+	return
+}
+
+// func Struct2StructByJson(to interface{}, from interface{}) {
+// 	Struct2Json(from)
+// 	Json2Struct(to, from.(string))
+// }
+
+func Json2Map(jsonStr string) (map[string]string, error) {
+	m := make(map[string]string)
+	err := json.Unmarshal([]byte(jsonStr), &m)
+	if err != nil {
+		fmt.Printf("Unmarshal with error: %+v\n", err)
+		return nil, err
+	}
+
+	for k, v := range m {
+		fmt.Printf("%v: %v\n", k, v)
+	}
+
+	return m, nil
+}
+
+// Copy 从一个结构体复制到另一个结构体
+func CopyX(from, to interface{}) error {
+	b, err := json.Marshal(from)
+	if err != nil {
+		return err
+		// return errors.Wrap(err, "marshal from data err")
+	}
+
+	err = json.Unmarshal(b, to)
+	if err != nil {
+		return err
+		//return errors.Wrap(err, "unmarshal to data err")
+	}
+
+	return nil
+}
